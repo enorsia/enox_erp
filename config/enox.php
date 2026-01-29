e0b68384278d1bd81d832fccc1c4ab0e317df74a
Rasel Hasan <rasel.ict@pakiza.com>
2026-01-29 06:15:05 +0000

    auth

--- config/enox.php
+++ config/enox.php
@@ -2,10 +2,12 @@
 
 return [
     'base_url' => env('ENOX_API_BASE_URL'),
+    'api_key' => env('ENOX_API_KEYS'),
     'timeout' => 10,
     'retry' => 2,
     'headers' => [
         'Accept' => 'application/json',
+        'X-INTERNAL-KEY' => env('ENOX_API_KEY'),
     ],
     'endpoints' => [
         'fabrications' => 'fabrications',
